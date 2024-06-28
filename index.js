5cbdea89b87c8583ece4f7a3413b75589c7a2b12
silly_silvy <silvyespindola2004@gmail.com>
2024-06-28 17:20:23 -0300

    criação de máscara para o campo de CPF do formulário utilizando jquery mask

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
